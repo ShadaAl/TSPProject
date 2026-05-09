c8fc846fc5d5a9e6130d2372696b147fe7c38548
ShadaAl <133864767+ShadaAl@users.noreply.github.com>
2026-05-09 18:00:02 +0300

    Update SimulatedAnnealingSolver.java

--- src/main/java/tsp/SimulatedAnnealingSolver.java
+++ src/main/java/tsp/SimulatedAnnealingSolver.java
@@ -31,7 +31,42 @@ public class SimulatedAnnealingSolver {
 		
 		
 		// TODO: Implement Simulated Annealing algorithm solution here.
-		
+        
+        double temperature = initialTemperature;
+        double currentDistance = TSPUtils.totalDistance(currentTour);
+        double bestDistance = currentDistance;
+        Random random = new Random();
+        iterationsUsed = 0;
+
+        while (iterationsUsed < maxIterations && temperature > 1) {
+            int index1 = random.nextInt(currentTour.size());
+            int index2 = random.nextInt(currentTour.size());
+
+            List<City> neighborTour = new ArrayList<>(currentTour);
+            Collections.swap(neighborTour, index1, index2);
+
+            double neighborDistance = TSPUtils.totalDistance(neighborTour);
+
+            //  (Acceptance Probability)
+            if (neighborDistance < currentDistance) {
+                currentTour = new ArrayList<>(neighborTour);
+                currentDistance = neighborDistance;
+            } else {
+                double acceptanceProb = Math.exp((currentDistance - neighborDistance) / temperature);
+                if (acceptanceProb > random.nextDouble()) {
+                    currentTour = new ArrayList<>(neighborTour);
+                    currentDistance = neighborDistance;
+                }
+            }
+
+            if (currentDistance < bestDistance) {
+                bestTour = new ArrayList<>(currentTour);
+                bestDistance = currentDistance;
+            }
+
+            temperature *= (1 - coolingRate);
+            iterationsUsed++;
+        }
 
 		return bestTour;
 	}
