72391e906cce50833a1d638c023c72a3458a2d1a
ShadaAl <133864767+ShadaAl@users.noreply.github.com>
2026-05-09 17:56:09 +0300

    Update City.java

--- src/main/java/tsp/City.java
+++ src/main/java/tsp/City.java
@@ -1,1 +1,34 @@
+package tsp;
 
+/**
+ *  a city model class
+ * @author Seham Alharbi
+ */
+public class City {
+	private int id;
+	private double x;
+	private double y;
+
+	public City(int id, double x, double y) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	@Override
+	public String toString() {
+		return id + " (" + x + ", " + y + ")";
+	}
+}
